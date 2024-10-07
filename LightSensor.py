fc6153868d5869fbbde07bd0fe79b0d7e814ad89
WiktorChargeEuropa <wiktor@chargeeuropa.com>
2024-10-07 12:14:55 +0200

    Update LightSensor.py

--- LightSensor.py
+++ LightSensor.py
@@ -54,6 +54,7 @@ def set_brightness(value):
     try:
             command = f"sudo ddcutil -d 1 setvcp 10 {value}"
             subprocess.call(command, shell=True)
+            print(f"Brightness set to {value}")
     except Exception as e:
         if debugMode:
             print(f"Error setting brightness using xrandr: {e}")
